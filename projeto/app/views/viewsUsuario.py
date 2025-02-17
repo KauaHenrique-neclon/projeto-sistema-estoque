4de021a44399289f2d36446720772a381009409b
KauaHenrique-neclon <brandokaua@gmail.com>
2025-02-16 21:46:09 -0300

    up da views usuario

--- projeto/app/views/viewsUsuario.py
+++ projeto/app/views/viewsUsuario.py
@@ -2,21 +2,29 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 from ..models import Usuario
+import re
 
 
 def index(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
         senha = request.POST.get('senha')
-        try:
-            novoUsuario = Usuario(
-                username = nome,
-                password = senha
-            )
-            novoUsuario.save()
-            return messages.success(request, 'Usuario criado')
-        except:
-            return messages.error(request, 'Erro ao criar um usuario')
+        padrão = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|gov|org)$"
+        if re.match(padrão, nome, re.IGNORECASE):
+            if senha >= 8:    
+                try:
+                    novoUsuario = Usuario(
+                        username = nome,
+                        password = senha
+                    )
+                    novoUsuario.save()
+                    return messages.success(request, 'Usuario criado')
+                except:
+                    return messages.error(request, 'Erro ao criar um usuario')
+            else:
+                return messages.error(request, 'Senha deve ter mais de 8 caracteres')
+        else:
+            return messages.error(request, 'Senha deve ser email')
     dadosUsuario = Usuario.objects.all()
     contexto = {'usuario':dadosUsuario}
     return render(request, 'usuario/usuario.html', contexto)
